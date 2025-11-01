fd48b9c46669bbe489cd68877574139e9ddf94e7
AshBots <ashworldco@gmail.com>
2025-11-01 14:28:35 +0500

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAAFqbEIL3TOAcFmsxoqltJfrtfE2sOXVs",
+  authDomain: "links-dm-pro.firebaseapp.com",
+  projectId: "links-dm-pro",
+  storageBucket: "links-dm-pro.firebasestorage.app",
+  messagingSenderId: "965082307073",
+  appId: "1:965082307073:web:78ea49e4c5888852307e00",
+  measurementId: "G-QVH0R5D92B"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
